1adf8f0c61cd17d1cff33cc7eb0057681d68346e
Adrian Aburto <A01731751@itesm.mx>
2021-04-21 23:54:59 -0500

    colpos-api

---
+++ src/test/java/com/example/colposapi/ColposApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.colposapi;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColposApiApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
